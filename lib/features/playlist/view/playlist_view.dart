f4cb0f9180bf12cbf9e215a04dfe28bb32dc8ee4
shijin thottiyil <115061533+shijinthottiyil@users.noreply.github.com>
2023-12-21 17:38:12 +0530

    PlaylistView updated and code cleaned

--- lib/features/playlist/view/playlist_view.dart
+++ lib/features/playlist/view/playlist_view.dart
@@ -48,121 +48,9 @@ class _PlaylistViewState extends State<PlaylistView> {
   @override
   Widget build(BuildContext context) {
     const duration = Duration(milliseconds: 300);
-    // final c = Get.put()
 
-    ///Old Code used at the time of transparent bg.
-
-    // return Stack(
-    //   fit: StackFit.expand,
-    //   children: [
-    //     ImageFiltered(
-    //       imageFilter: ImageFilter.blur(
-    //         sigmaX: 2,
-    //         sigmaY: 2,
-    //       ),
-    //       child: Image.network(
-    //         widget.playlistImg ?? '',
-    //         fit: BoxFit.fill,
-    //       ),
-    //     ),
-    //     Scaffold(
-    //       appBar: AppBar(
-    //         title: Text(widget.playlistName ?? ''),
-    //         elevation: 0.0,
-    //         backgroundColor: Colors.transparent,
-    //         leading: IconButton(
-    //           onPressed: () {
-    //             Get.back();
-    //           },
-    //           icon: const Icon(CupertinoIcons.back),
-    //         ),
-    //       ),
-    //       backgroundColor: Colors.transparent,
-    //       body: NotificationListener<UserScrollNotification>(
-    //         onNotification: (notification) {
-    //           final ScrollDirection direction = notification.direction;
-    //           setState(() {
-    //             if (direction == ScrollDirection.reverse) {
-    //               _showFab = false;
-    //             } else if (direction == ScrollDirection.forward) {
-    //               _showFab = true;
-    //             }
-    //           });
-    //           return true;
-    //         },
-    //         child: Obx(
-    //           () => ListView.builder(
-    //             itemBuilder: (context, index) {
-    //               var data = _playlistC.playlist.playlistList[index];
-    //               return Row(
-    //                 crossAxisAlignment: CrossAxisAlignment.center,
-    //                 children: [
-    //                   Padding(
-    //                     padding: const EdgeInsets.all(8.0),
-    //                     child: Text('${index + 1}.'),
-    //                   ),
-    //                   Expanded(
-    //                     child: ListTile(
-    //                       onTap: () {
-    //                         _playlistC.playSelected(index);
-    //                       },
-    //                       contentPadding: EdgeInsets.all(10.r),
-    //                       leading: ClipRRect(
-    //                         borderRadius: BorderRadius.circular(10.r),
-    //                         child: FadeInImage(
-    //                           placeholder: AssetImage(AppAssets.kLenin),
-    //                           image:
-    //                               NetworkImage(data.thumbnail?.last.url ?? ''),
-    //                           imageErrorBuilder: (context, error, stackTrace) =>
-    //                               Image.asset(
-    //                             AppAssets.kLenin,
-    //                             width: 60.w,
-    //                             height: 60.w,
-    //                             fit: BoxFit.cover,
-    //                           ),
-    //                           fit: BoxFit.cover,
-    //                           placeholderFit: BoxFit.cover,
-    //                           width: 60.w,
-    //                           height: 60.w,
-    //                         ),
-    //                       ),
-    //                       title: Text(
-    //                         data.title ?? 'title',
-    //                         style: const TextStyle(
-    //                             overflow: TextOverflow.ellipsis),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                 ],
-    //               );
-    //             },
-    //             itemCount: _playlistC.playlist.playlistList.length,
-    //           ),
-    //         ),
-    //       ),
-    //       floatingActionButton: AnimatedSlide(
-    //         duration: duration,
-    //         offset: _showFab ? Offset.zero : const Offset(0, 2),
-    //         child: AnimatedOpacity(
-    //           duration: duration,
-    //           opacity: _showFab ? 1 : 0,
-    //           child: FloatingActionButton(
-    //             elevation: 10,
-    //             backgroundColor: AppColors.kBlack,
-    //             onPressed: () {
-    //               _playlistC.playAll();
-    //             },
-    //             child: const Icon(Icons.play_arrow_rounded),
-    //           ),
-    //         ),
-    //       ),
-    //     ),
-    //   ],
-    // );
-
-    ///Trying out new Playlist Screen Design.
     return Scaffold(
-      backgroundColor: AppColors.kWhite,
+      // backgroundColor: AppColors.kWhite,
       body: Stack(
         children: [
           Column(
@@ -217,10 +105,10 @@ class _PlaylistViewState extends State<PlaylistView> {
                               padding: const EdgeInsets.all(8.0),
                               child: Text(
                                 '${index + 1}.',
-                                style: AppTypography.kSecondary.copyWith(
-                                  color: AppColors.kBlack,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
+                                // style: AppTypography.kSecondary.copyWith(
+                                //   color: AppColors.kBlack,
+                                //   overflow: TextOverflow.ellipsis,
+                                // ),
                               ),
                             ),
                             Expanded(
@@ -252,7 +140,7 @@ class _PlaylistViewState extends State<PlaylistView> {
                                 title: Text(
                                   data.title ?? 'title',
                                   style: AppTypography.kRegular13.copyWith(
-                                    color: AppColors.kBlack,
+                                    // color: AppColors.kBlack,
                                     overflow: TextOverflow.ellipsis,
                                   ),
                                 ),
